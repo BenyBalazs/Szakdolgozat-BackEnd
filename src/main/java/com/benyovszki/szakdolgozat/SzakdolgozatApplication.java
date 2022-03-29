7b67e39beaedde74a438563f5019b0ecbdb62d38
BenyBalazs <gnuki2000@gmail.com>
2022-03-29 16:35:13 +0200

    minor fix

--- src/main/java/com/benyovszki/szakdolgozat/SzakdolgozatApplication.java
+++ src/main/java/com/benyovszki/szakdolgozat/SzakdolgozatApplication.java
@@ -25,70 +25,68 @@ import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @AllArgsConstructor
-@NoArgsConstructor
 public class SzakdolgozatApplication {
 
-	private UserRepository userRepository;
-	private CategoryRepository categoryRepository;
-	private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private CategoryRepository categoryRepository;
+    private PasswordEncoder passwordEncoder;
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper mapper = new ModelMapper();
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
 
-		mapper.typeMap(Transaction.class, TransactionEntityType.class).addMappings(mapping -> {
-			mapping.map(source -> source.getOwner().getUsername(), TransactionEntityType::setOwner);
-			mapping.map(source -> source.getCategory().getId(), TransactionEntityType::setCategoryId);
-			mapping.map(source -> source.getCategory().getName(), TransactionEntityType::setCategoryName);
-		});
+        mapper.typeMap(Transaction.class, TransactionEntityType.class).addMappings(mapping -> {
+            mapping.map(source -> source.getOwner().getUsername(), TransactionEntityType::setOwner);
+            mapping.map(source -> source.getCategory().getId(), TransactionEntityType::setCategoryId);
+            mapping.map(source -> source.getCategory().getName(), TransactionEntityType::setCategoryName);
+        });
 
-		mapper.addMappings(new PropertyMap<TransactionEntityType, Transaction>() {
-			@Override
-			protected void configure() {
-				skip(destination.getDateOfAdd());
-				skip(destination.getId());
-				skip(destination.getOwner());
-				skip(destination.getCategory());
-			}
-		});
+        mapper.addMappings(new PropertyMap<TransactionEntityType, Transaction>() {
+            @Override
+            protected void configure() {
+                skip(destination.getDateOfAdd());
+                skip(destination.getId());
+                skip(destination.getOwner());
+                skip(destination.getCategory());
+            }
+        });
 
-		mapper.addMappings(new PropertyMap<CategoryEntityType, Category>() {
-			@Override
-			protected void configure() {
-				skip(destination.getId());
-			}
-		});
+        mapper.addMappings(new PropertyMap<CategoryEntityType, Category>() {
+            @Override
+            protected void configure() {
+                skip(destination.getId());
+            }
+        });
 
-		return mapper;
-	}
+        return mapper;
+    }
 
-	@PostConstruct
-	@Transactional
-	public void createBasicData() {
-		try {
-			User admin = User.builder()
-					.username("admin")
-					.email("admin@admin.hu")
-					.password(passwordEncoder.encode("admin"))
-					.build();
-			userRepository.save(admin);
-			categoryRepository.save(Category.builder().name("Fizetés").transactionType(TransactionType.INCOME).build());
-			categoryRepository.save(Category.builder().name("Bevásárlás").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Ajándék").transactionType(TransactionType.INCOME).build());
-			categoryRepository.save(Category.builder().name("Egyéb bevétel").transactionType(TransactionType.INCOME).build());
-			categoryRepository.save(Category.builder().name("Otthon").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Étterem").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Család").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Magán").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Öltözködés").transactionType(TransactionType.EXPENSE).build());
-			categoryRepository.save(Category.builder().name("Szórakozás").transactionType(TransactionType.EXPENSE).build());
-		}catch (Exception e) {
-			System.out.println("össze szartam magam bocsi");
-		}
-	}
+    @PostConstruct
+    public void createBasicData() {
+        try {
+            User admin = User.builder()
+                    .username("admin")
+                    .email("admin@admin.hu")
+                    .password(passwordEncoder.encode("admin"))
+                    .build();
+            userRepository.save(admin);
+            categoryRepository.save(Category.builder().name("Fizetés").transactionType(TransactionType.INCOME).build());
+            categoryRepository.save(Category.builder().name("Bevásárlás").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Ajándék").transactionType(TransactionType.INCOME).build());
+            categoryRepository.save(Category.builder().name("Egyéb bevétel").transactionType(TransactionType.INCOME).build());
+            categoryRepository.save(Category.builder().name("Otthon").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Étterem").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Család").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Magán").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Öltözködés").transactionType(TransactionType.EXPENSE).build());
+            categoryRepository.save(Category.builder().name("Szórakozás").transactionType(TransactionType.EXPENSE).build());
+        } catch (Exception e) {
+            System.out.println("Data already added");
+        }
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SzakdolgozatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SzakdolgozatApplication.class, args);
+    }
 
 }
